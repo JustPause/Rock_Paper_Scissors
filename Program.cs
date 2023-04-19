0eb70f66f27e6f7d11d3bb9d142eb695352f0cb5
JustPause <JustinasLOLT@GMAIL.COM>
2023-04-20 00:53:04 +0300

    Finish. Done. it is done

--- Program.cs
+++ Program.cs
@@ -8,88 +8,133 @@
             int RandomInt;
             int Score1 = 0;
             int Score2 = 0;
+            bool Play = true;
 
-            System.Console.Write("Choose, A move. Rock || Paper || Scissors: ");
-            Input1Player = Console.ReadLine();
-            Input1Player = Input1Player.ToUpper();
-
-            System.Random GetRand = new System.Random();
-
-            RandomInt = GetRand.Next(1, 4);
-
-            string[] Choses = new string[] { "ROCK", "PAPER", "SCISSORS" };
-
-
-
-            switch (RandomInt)
+            while (Play)
             {
-                case 1:
-                    Input2Player = Choses[RandomInt - 1];
+                Score1 = 0; Score2 = 0;
+                while (Score1 < 3 && Score2 < 3)
+                {
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+                    System.Console.Write("Choose, A move. Rock || Paper || Scissors: ");
+                    Input1Player = Console.ReadLine();
+                    Input1Player = Input1Player.ToUpper();
 
-                    if (Input1Player == Choses[RandomInt - 1])
+                    System.Random GetRand = new System.Random();
+
+                    RandomInt = GetRand.Next(1, 4);
+
+                    string[] Choses = new string[] { "ROCK", "PAPER", "SCISSORS" };
+
+                    switch (RandomInt)
                     {
-                        System.Console.WriteLine("GG. Is a Draw");
-                    }
-                    else if (Input1Player == Choses[RandomInt])
-                    {
-                        System.Console.WriteLine("Player wins!!");
-                        Score1++;
-                    }
-                    else if (Input1Player == Choses[RandomInt + 1])
-                    {
-                        System.Console.WriteLine("AI wins!!");
-                        Score2++;
-                    }
-                    break;
+                        case 1:
+                            Input2Player = Choses[RandomInt - 1];
 
-                case 2:
-                    Input2Player = Choses[RandomInt - 1];
+                            System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+                            if (Input1Player == Choses[RandomInt - 1])
+                            {
+                                System.Console.WriteLine("GG. Is a Draw");
+                            }
+                            else if (Input1Player == Choses[RandomInt])
+                            {
+                                System.Console.WriteLine("Player wins!!");
+                                Score1++;
+                            }
+                            else if (Input1Player == Choses[RandomInt + 1])
+                            {
+                                System.Console.WriteLine("AI wins!!");
+                                Score2++;
+                            }
+                            System.Console.WriteLine("Player = {0}, AI = {1}", Score1, Score2);
+                            break;
 
-                    if (Input1Player == Choses[RandomInt - 1])
-                    {
-                        System.Console.WriteLine("GG. Is a Draw");
+                        case 2:
+                            Input2Player = Choses[RandomInt - 1];
+
+                            System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+
+                            if (Input1Player == Choses[RandomInt - 1])
+                            {
+                                System.Console.WriteLine("GG. Is a Draw");
+                            }
+                            else if (Input1Player == Choses[RandomInt])
+                            {
+                                System.Console.WriteLine("Player wins!!");
+                                Score1++;
+                            }
+                            else if (Input1Player == Choses[RandomInt - 2])
+                            {
+                                System.Console.WriteLine("AI wins!!");
+                                Score2++;
+                            }
+                            System.Console.WriteLine("Player = {0}, AI = {1}", Score1, Score2);
+                            break;
+
+                        case 3:
+
+                            Input2Player = Choses[RandomInt - 1];
+
+                            System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+
+                            if (Input1Player == Choses[RandomInt - 1])
+                            {
+                                System.Console.WriteLine("GG. Is a Draw");
+                            }
+                            else if (Input1Player == Choses[RandomInt - 3])
+                            {
+                                System.Console.WriteLine("Player wins!!");
+                                Score1++;
+                            }
+                            else if (Input1Player == Choses[RandomInt - 2])
+                            {
+                                System.Console.WriteLine("AI wins!!");
+                                Score2++;
+                            }
+                            System.Console.WriteLine("Player = {0}, AI = {1}", Score1, Score2);
+                            break;
+
+                        default:
+                            System.Console.WriteLine("Somefing went worong.");
+                            break;
                     }
-                    else if (Input1Player == Choses[RandomInt])
-                    {
-                        System.Console.WriteLine("Player wins!!");
-                        Score1++;
-                    }
-                    else if (Input1Player == Choses[RandomInt - 2])
-                    {
-                        System.Console.WriteLine("AI wins!!");
-                        Score2++;
-                    }
-                    break;
+                }
+                if (Score1 == 3)
+                {
+                    System.Console.WriteLine("Player has won. Nice");
+                }
+                else if (Score2 == 3)
+                {
+                    System.Console.WriteLine("AI has won. Bad");
+                }
+                else
+                {
 
-                case 3:
+                }
 
-                    Input2Player = Choses[RandomInt - 1];
+                while (true)
+                {
+                    System.Console.WriteLine("Play again? [Y/n]");
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
-
-                    if (Input1Player == Choses[RandomInt - 1])
+                    string PlayGame = "y";
+                    PlayGame = System.Console.ReadLine();
+                    PlayGame = PlayGame.ToUpper();
+                    if (PlayGame == "Y")
                     {
-                        System.Console.WriteLine("GG. Is a Draw");
+                        Play = true;
+                        break;
                     }
-                    else if (Input1Player == Choses[RandomInt - 2])
+                    else if (PlayGame == "N")
                     {
-                        System.Console.WriteLine("Player wins!!");
-                        Score1++;
+                        Play = false;
+                        break;
                     }
-                    else if (Input1Player == Choses[RandomInt])
+                    else
                     {
-                        System.Console.WriteLine("AI wins!!");
-                        Score2++;
+                        System.Console.WriteLine("Wrong input");
                     }
-                    break;
-
-                default:
-                    System.Console.WriteLine("Somefing went worong.");
-                    break;
+                }
             }
         }
     }
