0a3bfe3f2443413d9c5987288357d049e394fd18
JustPause <JustinasLOLT@GMAIL.COM>
2023-04-19 21:38:35 +0300

    Big Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RockPaperScissors
+{
+    class TheProgram
+    {
+        static void Main(string[] arg)
+        {
+            string Input1Player, Input2Player;
+            int RandomInt;
+            int Score1 = 0;
+            int Score2 = 0;
+
+            System.Console.Write("Choose, A move. Rock: Paper: Scissors:       ");
+            Input1Player = Console.ReadLine();
+            Input1Player = Input1Player.ToUpper();
+
+            System.Random GetRand = new System.Random();
+
+            RandomInt = GetRand.Next(1, 4);
+
+            string[] Choses = new string[] { "ROCK", "PAPER", "SCISSORS"};
+
+            switch (RandomInt)
+            {
+                case 1:
+                    Input2Player = Choses[RandomInt - 1];
+
+                    System.Console.WriteLine("The AI has chosen a {Choses[RandomInt - 1]}");
+
+                    if (Input1Player == Choses[RandomInt - 1])
+                    {
+                        System.Console.WriteLine("GG. Is a Draw");
+                    }
+                    else if (Input1Player == Choses[RandomInt])
+                    {
+                        System.Console.WriteLine("Player wins!!");
+                        Score1++;
+                    }
+                    else if (Input1Player == Choses[RandomInt + 1])
+                    {
+                        System.Console.WriteLine("AI wins!!");
+                        Score2++;
+                    }
+                    break;
+
+                case 2:
+                    Input2Player = Choses[RandomInt - 1];
+
+                    System.Console.WriteLine("The AI has chosen a {Choses[RandomInt - 1]}");
+
+                    if (Input1Player == Choses[RandomInt - 1])
+                    {
+                        System.Console.WriteLine("GG. Is a Draw");
+                    }
+                    else if (Input1Player == Choses[RandomInt])
+                    {
+                        System.Console.WriteLine("Player wins!!");
+                        Score1++;
+                    }
+                    else if (Input1Player == Choses[RandomInt - 2])
+                    {
+                        System.Console.WriteLine("AI wins!!");
+                        Score2++;
+                    }
+                    break;
+
+                case 3:
+
+                    Input2Player = Choses[RandomInt - 1];
+
+                    System.Console.WriteLine("The AI has chosen a {Choses[RandomInt - 1]}");
+
+                    if (Input1Player == Choses[RandomInt - 1])
+                    {
+                        System.Console.WriteLine("GG. Is a Draw");
+                    }
+                    else if (Input1Player == Choses[RandomInt - 2])
+                    {
+                        System.Console.WriteLine("Player wins!!");
+                        Score1++;
+                    }
+                    else if (Input1Player == Choses[RandomInt])
+                    {
+                        System.Console.WriteLine("AI wins!!");
+                        Score2++;
+                    }
+                    break;
+
+                default:
+                    System.Console.WriteLine("Somefing went worong.");
+                    break;
+            }
+        }
+    }
+}
