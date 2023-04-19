96e147a9ba07a5d665e94702d9082d5d28d69ecb
JustPause <JustinasLOLT@GMAIL.COM>
2023-04-19 23:07:14 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,89 +1,98 @@
 ﻿namespace RockPaperScissors
 {
+    static class Int_Int
+    {
+        public string Input1Player, Input2Player;
+        public static int RandomInt;
+        public static int Score1 = 0;
+        public static int Score2 = 0;
+    }
     class TheProgram
     {
         static void Main(string[] arg)
         {
-            string Input1Player, Input2Player;
-            int RandomInt;
-            int Score1 = 0;
-            int Score2 = 0;
+
+
+            while (Int_Int.Score1 < 3 && Int_Int.Score2 < 3)
+            {
+
+            }
 
             System.Console.Write("Choose, A move. Rock || Paper || Scissors: ");
-            Input1Player = Console.ReadLine();
-            Input1Player = Input1Player.ToUpper();
+            Int_Int.Input1Player = Console.ReadLine();
+            Int_Int.Input1Player = Int_Int.Input1Player.ToUpper();
+        }
 
+        static void F_switch()
+        {
             System.Random GetRand = new System.Random();
 
-            RandomInt = GetRand.Next(1, 4);
+            Int_Int.RandomInt = GetRand.Next(1, 4);
 
             string[] Choses = new string[] { "ROCK", "PAPER", "SCISSORS" };
-
-
-
-            switch (RandomInt)
+            switch (Int_Int.RandomInt)
             {
                 case 1:
-                    Input2Player = Choses[RandomInt - 1];
+                    Int_Int.Input2Player = Choses[Int_Int.RandomInt - 1];
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+                    System.Console.WriteLine("The AI has chosen a {0}", Choses[Int_Int.RandomInt - 1]);
 
-                    if (Input1Player == Choses[RandomInt - 1])
+                    if (Int_Int.Input1Player == Choses[Int_Int.RandomInt - 1])
                     {
                         System.Console.WriteLine("GG. Is a Draw");
                     }
-                    else if (Input1Player == Choses[RandomInt])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt])
                     {
                         System.Console.WriteLine("Player wins!!");
-                        Score1++;
+                        Int_Int.Score1++;
                     }
-                    else if (Input1Player == Choses[RandomInt + 1])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt + 1])
                     {
                         System.Console.WriteLine("AI wins!!");
-                        Score2++;
+                        Int_Int.Score2++;
                     }
                     break;
 
                 case 2:
-                    Input2Player = Choses[RandomInt - 1];
+                    Int_Int.Input2Player = Choses[Int_Int.RandomInt - 1];
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+                    System.Console.WriteLine("The AI has chosen a {0}", Choses[Int_Int.RandomInt - 1]);
 
-                    if (Input1Player == Choses[RandomInt - 1])
+                    if (Int_Int.Input1Player == Choses[Int_Int.RandomInt - 1])
                     {
                         System.Console.WriteLine("GG. Is a Draw");
                     }
-                    else if (Input1Player == Choses[RandomInt])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt])
                     {
                         System.Console.WriteLine("Player wins!!");
-                        Score1++;
+                        Int_Int.Score1++;
                     }
-                    else if (Input1Player == Choses[RandomInt - 2])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt - 2])
                     {
                         System.Console.WriteLine("AI wins!!");
-                        Score2++;
+                        Int_Int.Score2++;
                     }
                     break;
 
                 case 3:
 
-                    Input2Player = Choses[RandomInt - 1];
+                    Int_Int.Input2Player = Choses[Int_Int.RandomInt - 1];
 
-                    System.Console.WriteLine("The AI has chosen a {0}", Choses[RandomInt - 1]);
+                    System.Console.WriteLine("The AI has chosen a {0}", Choses[Int_Int.RandomInt - 1]);
 
-                    if (Input1Player == Choses[RandomInt - 1])
+                    if (Int_Int.Input1Player == Choses[Int_Int.RandomInt - 1])
                     {
                         System.Console.WriteLine("GG. Is a Draw");
                     }
-                    else if (Input1Player == Choses[RandomInt - 2])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt - 2])
                     {
                         System.Console.WriteLine("Player wins!!");
-                        Score1++;
+                        Int_Int.Score1++;
                     }
-                    else if (Input1Player == Choses[RandomInt])
+                    else if (Int_Int.Input1Player == Choses[Int_Int.RandomInt])
                     {
                         System.Console.WriteLine("AI wins!!");
-                        Score2++;
+                        Int_Int.Score2++;
                     }
                     break;
 
